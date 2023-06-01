cc71ff7410b363882ce8baaf2a20e2abfa504aa4
Zorigt Batzorigt <71284891+REVO-X@users.noreply.github.com>
2023-06-01 20:42:11 +0900

    Prototype

---
+++ ballCounter.ino
@@ -0,0 +1,23 @@
+// 定数の定義
+const int sensorPin = 0;    // 光センサーモジュールの出力ピン
+const int ballThreshold = 600;  // 玉を検出するしきい値
+
+// 変数の定義
+int ballCount = 0;  // 玉のカウンタ
+
+void setup() {
+  Serial.begin(9600);  // シリアル通信の開始
+  pinMode(sensorPin, INPUT);  // 光センサーモジュールのピンを入力モードに設定
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);  // 光センサーモジュールからの読み取り
+  
+  if (sensorValue > ballThreshold) {  // 玉を検出した場合
+    ballCount++;  // カウンタをインクリメント
+    Serial.println("Ball detected!");
+    Serial.print("Count: ");
+    Serial.println(ballCount);
+    delay(500);  // カウント間の遅延
+  }
+}
